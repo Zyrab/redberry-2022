74381ca4fab9354ca40abbc25cffb403c159f98b
Zurab <zurakrua@gmail.com>
2023-02-04 23:00:47 +0400

    ADD JS

---
+++ main.js
@@ -0,0 +1,8 @@
+
+//live text adding function
+function liveTextAdd(inputId,outputId){
+   var liveText=document.querySelector(inputId).value;
+   document.querySelector(outputId).innerHTML=liveText;
+}
+
+
