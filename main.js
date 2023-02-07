6752fb72e2390fa2185d7d56eb5cc86fbd886696
Zurab <zurakrua@gmail.com>
2023-02-07 15:16:34 +0400

    live text generator update

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
+// varibles
+const form = document.querySelector('form')
 
-//live text adding function
-function liveTextAdd(inputId,outputId){
-   var liveText=document.querySelector(inputId).value;
-   document.querySelector(outputId).innerHTML=liveText;
+
+
+
+// for text generation
+form.addEventListener('keyup', e =>{ 
+   //variables
+   var outpudId = e.target.id+'1'
+   var liveText = e.target.value 
+   var elementId = e.target
+   //--//
+   //live text generator      
+   document.getElementById(outpudId).innerHTML=liveText 
+   //--//
+})
+
+
+
+
+
+
+
+
+
+
+
+//functions
+
+
+//chek vlid input
+const validInput = (validation, elementId) => {
+   if (validation){
+       elementId.classList.add('success')
+       elementId.classList.remove('error')
+       
+   } else {
+       elementId.classList.add('error')
+       elementId.classList.remove('success')
+   }
 }
-
-
