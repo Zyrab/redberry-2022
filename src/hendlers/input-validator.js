f0e02bdd544514496b6f7170524937442598840c
zyrab <zurakrua@gmail.com>
2025-08-28 18:31:53 +0200

    feat: validates input areas on keyup

---
+++ src/hendlers/input-validator.js
@@ -0,0 +1,24 @@
+import { regex } from "./utils/text-regex.js";
+import toggleValidationUI from "./utils/toggle-validation-ui.js";
+
+export default function inputValidator(e) {
+  const input = e.target;
+  const filter = input.dataset.validator;
+  if (!filter) return;
+
+  let isValid = false;
+  let toggleClasses = true;
+
+  if (regex[filter]) {
+    isValid = regex[filter].test(input.value.trim());
+  } else if (filter === "date") {
+    isValid = input.valueAsNumber > 0 || input.value !== "";
+  } else if (filter === "select") {
+    isValid = input.valueAsNumber > 0 || input.value !== "";
+  } else if (filter === "file") {
+    isValid = input.files.length > 0;
+    toggleClasses = false;
+  }
+
+  toggleValidationUI(input, isValid, { toggleClasses });
+}
