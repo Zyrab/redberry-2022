d6f2e2353d91942c55f7926bd9581d8f2a4342a6
zyrab <zurakrua@gmail.com>
2025-08-30 02:48:05 +0200

    ref: file system Reorganization

--- forAside.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    var icons = document.querySelector('.contact')
-
-    icons.addEventListener('change' , e =>{
-
-        console.log(icons.childNodes[3].textContent)
-    })
-}
