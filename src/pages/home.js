c4a433ca2a198a626e959261e8477f0f7a99ac96
zyrab <zurakrua@gmail.com>
2025-08-30 03:25:08 +0200

    ref: fixed srces

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,18 +5,21 @@ export default function createHome() {
   return Domo("main").child([
     Domo("header")
       .cls(" px-2.5")
-      .child([Domo("img").cls("py-1.5").attr({ src: "/imiges/logo-text.png", alt: "Redberry logo" }), Domo("hr")]),
+      .child([
+        Domo("img").cls("py-1.5").attr({ src: "/public/images/logo-text.png", alt: "Redberry logo" }),
+        Domo("hr"),
+      ]),
 
     Domo("div")
       .cls("button-container")
       .child([
         Domo("button")
-          .txt("რეზიუმეს დამატება")
+          .txt("ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ")
           .on("click", () => {
             Router.goTo("/generator");
           }),
       ]),
-    Domo("img").cls("bg-img").attr({ src: "/imiges/shutterstock.png" }),
-    Domo("img").cls("bg-logo").attr({ src: "/imiges/logo-circle.png" }),
+    Domo("img").cls("bg-img").attr({ src: "/public/images/shutterstock.png" }),
+    Domo("img").cls("bg-logo").attr({ src: "/public/images/logo-circle.png" }),
   ]);
 }
